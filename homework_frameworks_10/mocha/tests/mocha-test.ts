16520881d7491a269ec3ae2efabc43469b16679d
Nataliia Molotko <developercss@gmail.com>
2025-02-28 20:53:10 +0100

    Fixes for top level in mocha-test and remove node modules for homework11

--- homework_frameworks_10/mocha/tests/mocha-test.ts
+++ homework_frameworks_10/mocha/tests/mocha-test.ts
@@ -1,92 +1,60 @@
 import { expect } from 'chai';
 import { Animal, Dog, Cat, PetOwner } from 'src\\classes';
 
-describe('Animal class tests', () => {
-    it('should make sound correctly', () => {
-        const animal = new Animal('Generic Animal', 5);
+describe('Top level describe', () => {
 
-        const originalLog = console.log;
-        let logOutput = '';
+    const originalLog = console.log;
+    let logOutput: string[];
+
+    beforeEach(() => {
+        logOutput = [];
         console.log = (message: string) => {
-            logOutput = message;
+            logOutput.push(message);
         };
+    });
 
-        animal.makeSound();
-
-        expect(logOutput).to.equal('Generic Animal makes a sound!');
+    afterEach(() => {
         console.log = originalLog;
     });
-});
 
-describe('Dog class tests', () => {
+    it('should make sound correctly', () => {
+        const animal = new Animal('Generic Animal', 5);
+        animal.makeSound();
+
+        expect(logOutput[0]).to.equal('Generic Animal makes a sound!');
+    });
+
     it('should make a barking sound', () => {
         const dog = new Dog('Lucy', 5);
-
-        const originalLog = console.log;
-        let logOutput = '';
-        console.log = (message: string) => {
-            logOutput = message;
-        };
-
         dog.makeSound();
 
-        expect(logOutput).to.equal('Lucy barks!');
-        console.log = originalLog;
+        expect(logOutput[0]).to.equal('Lucy barks!');
     });
 
     it('should allow petting the dog', () => {
         const dog = new Dog('Lucy', 5);
-
-        const originalLog = console.log;
-        let logOutput = '';
-        console.log = (message: string) => {
-            logOutput = message;
-        };
-
         dog.pet();
 
-        expect(logOutput).to.equal('You pet Lucy, the dog!');
-        console.log = originalLog;
+        expect(logOutput[0]).to.equal('You pet Lucy, the dog!');
     });
-});
 
-describe('Cat class tests', () => {
     it('should make a meowing sound', () => {
         const cat = new Cat('Patty', 3);
-
-        const originalLog = console.log;
-        let logOutput = '';
-        console.log = (message: string) => {
-            logOutput = message;
-        };
-
         cat.makeSound();
 
-        expect(logOutput).to.equal('Patty meows!');
-        console.log = originalLog;
+        expect(logOutput[0]).to.equal('Patty meows!');
     });
 
     it('should allow petting the cat', () => {
         const cat = new Cat('Patty', 3);
-
-        const originalLog = console.log;
-        let logOutput = '';
-        console.log = (message: string) => {
-            logOutput = message;
-        };
-
         cat.pet();
 
-        expect(logOutput).to.equal('You pet Patty, the cat!');
-        console.log = originalLog;
+        expect(logOutput[0]).to.equal('You pet Patty, the cat!');
     });
-});
 
-describe('PetOwner class tests', () => {
     it('should adopt a pet correctly', () => {
         const petOwner = new PetOwner('Alex');
         const dog = new Dog('Lucy', 5);
-
         petOwner.adoptPet(dog);
 
         expect(petOwner['pets']).to.include(dog);
@@ -99,34 +67,17 @@ describe('PetOwner class tests', () => {
 
         petOwner.adoptPet(dog);
         petOwner.adoptPet(cat);
-
-        const originalLog = console.log;
-        const logOutput: string[] = [];
-        console.log = (message: string) => {
-            logOutput.push(message);
-        };
-
         petOwner.showPets();
 
         expect(logOutput).to.include('Lucy is a Dog');
         expect(logOutput).to.include('Patty is a Cat');
-
-        console.log = originalLog;
     });
 
     it('should pet an animal correctly', () => {
         const petOwner = new PetOwner('Alex');
         const dog = new Dog('Lucy', 5);
-
-        const originalLog = console.log;
-        let logOutput = '';
-        console.log = (message: string) => {
-            logOutput = message;
-        };
-
         petOwner.petAnimal(dog);
 
-        expect(logOutput).to.equal('You pet Lucy, the dog!');
-        console.log = originalLog;
+        expect(logOutput[0]).to.equal('You pet Lucy, the dog!');
     });
 });
